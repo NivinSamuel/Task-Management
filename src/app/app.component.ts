b8c50562d9428efed5cebaebec6af88b038c6e14
NivinSamuel <samuelnivin96@gmail.com>
2024-08-26 23:31:14 +0530

    Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent,SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
